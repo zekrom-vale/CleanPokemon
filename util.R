135de9b71c42c64ad38e186ead78825778ad91d0
zekrom-vale <sgraven66@gmail.com>
2019-04-02 10:13:08 -0500

    Added more useful functions

--- util.R
+++ util.R
@@ -1,8 +1,20 @@
-# See how at https://dplyr.tidyverse.org/articles/programming.html
 require(dplyr);
-# This function returns thae values that are not distinct
-
 print("This small utility package was developed by zekrom_vale to fill in the gaps missing.");
+################################################################################
+#																	Elements
+################################################################################
+# See how at https://dplyr.tidyverse.org/articles/programming.html
+# This function returns thae values that are not distinct
+not_distinct=function(df, ...){
+	nd_vars=enquos(...);
+	df%>%
+		group_by(
+			!!!nd_vars
+		)%>%
+		filter(
+			reduce(c(!!!nd_vars), function(a,x)a&(n()>1), .init=TRUE)
+		);
+}
 not_distinct_=function(df, col, ...){
 	nd_vars=enquos(col,...);
 	nd_col=enquo(col);
@@ -21,18 +33,68 @@ not_distinct_=function(df, col, ...){
 		);
 }
 
-not_distinct=function(df, ...){
-	nd_vars=enquos(...);
-	df%>%
-		group_by(
-			!!!nd_vars
-		)%>%
-		filter(
-			reduce(c(!!!nd_vars), function(a,x)a&(n()>1), .init=TRUE)
-		);
+# Filter where NA
+filter_na=function(df, ...){
+	names=map(unlist(enquos(...)), .f=function(x)quo(is.na(!!x)));
+	filter(
+		df, !!!names
+	);
 }
 
-# Visulization functions
+# Filter where not NA
+filter_not_na=function(df, ...){
+	names=map(unlist(enquos(...)), .f=function(x)quo(!is.na(!!x)));
+	filter(
+		df, !!!names
+	);
+}
+################################################################################
+#																		Sets
+################################################################################
+
+# auto union, slices the tables down to match to union
+auto_union=function(x,y){
+	names=syms(
+		intersect(
+			names(x),
+			names(y)
+		)
+	);
+	names=map(names, function(x)enquo(x));
+	union(
+		select(x, !!!names),
+		select(y, !!!names)
+	)
+}
+# Automatic set difference
+auto_setdiff=function(x,y){
+	names=syms(
+		intersect(
+			names(x),
+			names(y)
+		)
+	);
+	names=map(names, function(x)enquo(x));
+	setdiff(
+		select(x, !!!names),
+		select(y, !!!names)
+	)
+}
+
+symdiff=function(A, B, ...){
+	setdiff(
+		union(
+			A, B, ...
+		),
+		intersect(
+			A, B, ...
+		)
+	);
+}
+
+################################################################################
+# 													Visulization functions
+################################################################################
 chunk=function(df,index=1, start=1, end=20){
 	slice(df,(start:end)+end*index)
 }
@@ -64,3 +126,4 @@ pan=function(df, index=2, start=1, end=12, frozen=NULL){
 v=function(.){
 	View(.)
 }
+
