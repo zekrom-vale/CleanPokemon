a33b855c0ea6efe0351164ac67be8717b630c1ce
zekrom-vale <sgraven66@gmail.com>
2019-04-05 21:52:28 -0500

    Now processes family and evolution

--- evolution/clean.R
+++ evolution/clean.R
@@ -1,5 +1,9 @@
 family=read_csv("evolution/EvolutionRaw - Family.csv");
 
+##################################################################################
+#																	Extract
+##################################################################################
+
 # Extract Dex into Dex and Dex_Suffix
 reg="\\s*#?(\\d+)?([[:alpha:]]+)?\\s*";
 into=c("Dex","Dex_Suffix");
@@ -71,4 +75,190 @@ family%<>%
 		Dex_2, Dex_Suffix_2, Name_2, Class_2, Condition_2, Sub_Condition_2, Level_2,
 		everything()
 );
+# Fix Espeon and Umbreon
+family%<>%
+	mutate(
+		Sub_Condition=case_when(
+			Name_2=="Espeon"~"night",
+			Name_2=="Umbreon"~"day",
+			TRUE~Sub_Condition
+		)
+	);
 
+##################################################################################
+#																	Split
+##################################################################################
+
+# Split seperate evolutions and families
+evolutions=family%>%
+	filter(!is.na(Condition)|!is.na(Sub_Condition)|!is.na(Level));
+
+family.o=family%>%
+	filter_na(Condition, Sub_Condition, Level);
+
+
+# Condence the Class and Dex suffix
+evolutions%<>%
+	mutate(
+		Class=case_when(
+			!is.na(Class)~Class,
+			!is.na(Class_2)~Class_2,
+			TRUE~NA_character_
+		),
+		Dex_Suffix=case_when(
+			!is.na(Dex_Suffix)~Dex_Suffix,
+			!is.na(Dex_Suffix_2)~Dex_Suffix_2,
+			!is.na(Dex_Suffix_3)~Dex_Suffix_3,
+			TRUE~NA_character_
+		),
+		Class_2=NULL,
+		Dex_Suffix_2=NULL,
+		Dex_Suffix_3=NULL
+	);
+
+
+# Split the table up
+evolutions1=evolutions%>%
+	select(1:11)%>%
+	rename(
+		Next_Dex=Dex_2,
+		Next_Name=Name_2
+	)%>%
+	add_column(
+		Previous_Dex=NA_integer_,
+		Previous_Name=NA_character_,
+		Stage=1L
+	);
+
+evolutions2=evolutions%>%
+	select(
+		-c(Condition, Sub_Condition, Level)
+	)%>%
+	rename(
+		Previous_Dex=Dex,
+		Previous_Name=Name,
+
+		Dex=Dex_2,
+		Name=Name_2,
+
+		Next_Dex=Dex_3,
+		Next_Name=Name_3,
+
+		Condition=Condition_2,
+		Sub_Condition=Sub_Condition_2,
+		Level=Level_2
+	)%>%
+	add_column(
+		Stage=2L
+	);
+
+evolutions3=evolutions%>%
+	select(
+		-c(Condition, Sub_Condition, Level, Level_2, Condition_2, Sub_Condition_2, Name, Dex)
+	)%>%
+	rename(
+		Previous_Dex=Dex_2,
+		Previous_Name=Name_2,
+
+		Dex=Dex_3,
+		Name=Name_3
+	)%>%
+	add_column(
+		Next_Dex=NA_integer_,
+		Next_Name=NA_character_,
+		Condition=NA_character_,
+		Sub_Condition=NA_character_,
+		Level=NA_integer_,
+		Stage=3L
+	);
+
+# Remove junk
+family.o%<>%
+	select(
+		-c(Condition, Condition_2, Level, Level_2, Sub_Condition, Sub_Condition_2)
+	);
+
+# Fix Class
+family.o%<>%
+	mutate(
+		Class=case_when(
+			!is.na(Class)~Class,
+			!is.na(Class_2)~Class_2,
+			TRUE~NA_character_
+		),
+		Class_2=NULL
+	);
+
+# Split family
+family=family.o%>%
+	select(
+		1:6
+	)%>%
+	union(
+		family.o%>%
+			select(
+				1:2,
+				Class,
+				Dex_2, Dex_Suffix_2, Name_2
+			)%>%
+			rename(
+				Dex=Dex_2,
+				Dex_Suffix=Dex_Suffix_2,
+				Name=Name_2
+			)
+	)%>%
+	union(
+		family.o%>%
+			select(
+				1:2,
+				Class,
+				Dex_3, Dex_Suffix_3, Name_3
+			)%>%
+			rename(
+				Dex=Dex_3,
+				Dex_Suffix=Dex_Suffix_3,
+				Name=Name_3
+			)
+	);
+
+
+##################################################################################
+#																	Union
+##################################################################################
+# Union with evolutions
+evolutions=evolutions1%>%
+	union(
+		evolutions2
+	)%>%
+	union(
+		evolutions3
+	);
+
+##################################################################################
+#																Remove Invalid
+##################################################################################
+evolutions%<>%
+	filter_not_na(
+		Dex, Name
+	);
+
+family%<>%
+	filter_not_na(
+		Dex, Name
+	);	# Many invalid
+
+##################################################################################
+#																Add evolutions to family
+##################################################################################
+family%<>%
+	union(
+		evolutions%>%
+			distinct(
+				Region, Family, Dex, Dex_Suffix, Name, Class
+			)
+	);#%>%not_distinct(Dex, Dex_Suffix, Name);# Nothing not distinct
+
+rm(evolutions1, evolutions2, evolutions3, family.o);
+
+write_csv(evolutions, "evolution/evolutions.csv",na="");
+write_csv(family, "evolution/family.csv",na="");
