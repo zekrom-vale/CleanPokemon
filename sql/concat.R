8925ba0476d596a412795f5d2a659aad04423b5b
zekrom-vale <sgraven66@gmail.com>
2019-05-06 18:40:14 -0500

    Removed spaces to allow for more tables, -100K chars

--- sql/concat.R
+++ sql/concat.R
@@ -9,8 +9,19 @@ files=read_csv("sql/driver.txt", col_names="File", comment="#")$File;
 write_file("", "sql/long.sql"); # Write nothing to the file
 # For all strings in files
 for(file in files){
+	# Read the contents of sql/<file>
+	text=read_file(paste0("sql/",file));
+	# Use technique to do nothing on capture 1 to prevent modification
+	# This is the only way I know how to do this, without a long workaround
+	text%<>%
+		gsub(
+			"(--[^\n]*\n|'.*?'|q'(.).*?\\2')|[ \t]*([,\\(\\)\\|])[ \t]*",
+			"\\1\\3",
+			.,
+			perl=TRUE # Non-perl is odd, .*? does not behave
+		);
 	write_file(# Append the folowing to sql/long.sql
-		read_file(paste0("sql/",file)), # Read the contents of sql/<file>
+		text,
 		"sql/long.sql",
 		append=TRUE # Don't overite the file, append it
 	);
